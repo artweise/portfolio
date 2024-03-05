17edcc824e26e2dae1ce3962d9d47e90328c60a1
artweise <art2wsxweise7ujm@gmail.com>
2024-03-05 23:25:36 +0100

    fix(viteConfig): fixed the issue with vite.config.js, the import was not right

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
 // export default defineConfig(({ mode }) => {
 //   const env = loadEnv(mode, process.cwd(), '');
 //   return {
@@ -15,7 +15,7 @@ import react from '@vitejs/plugin-react-swc';
 // alternative solution
 
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 const cherryPickedKeys = ['SOME_KEY_IN_YOUR_ENV_FILE', 'SOME_OTHER_KEY_IN_YOUR_ENV_FILE'];
 
