2c324eff7466c67870c9bc50b4fbf2db09bb62a4
artweise <art2wsxweise7ujm@gmail.com>
2024-03-05 22:31:15 +0100

    feat(HomePage): created a homepage with all sections, navbarlinks to every section, routes

---
+++ src/components/About/aboutStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { SPACING, FlexContainer } from '../../utilities/globalStyles';
+
+export const StyledAboutSection = styled(FlexContainer)`
+  height: 95vh;
+  justify-content: center-between;
+  align-items: center;
+  background-color: rgb(115, 139, 243);
+  font-size: 30px;
+`;
---
+++ src/components/Hero/heroStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { SPACING, FlexContainer } from '../../utilities/globalStyles';
+
+export const StyledHeroSection = styled(FlexContainer)`
+  height: 95vh;
+  justify-content: center-between;
+  align-items: center;
+  background-color: rgb(214, 223, 135);
+  font-size: 30px;
+`;
