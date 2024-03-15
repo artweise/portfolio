d22ba229d8bf3f148b6ccc7314adc55fb9fd407f
artweise <art2wsxweise7ujm@gmail.com>
2024-03-15 10:37:38 +0100

    refactor(Navbar): added a sliding link with text, that changes after hover it

--- src/components/Navbar/navbarStyle.js
+++ src/components/Navbar/navbarStyle.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import { SPACING, FlexContainer } from '../../utilities/globalStyles';
 
 export const StyledNavbar = styled(FlexContainer)`
+  display: flex;
   height: 68px;
   justify-content: space-between;
   align-items: center;
   padding-inline: ${SPACING[4]};
   background-color: #272835;
   color: white;
+  font-weight: 300;
   z-index: 2;
   a {
     text-decoration: none;
@@ -18,4 +21,23 @@ export const StyledNavbar = styled(FlexContainer)`
     display: flex;
     gap: ${SPACING[3]};
   }
+`;
+
+export const StyledSlidingLinkContainer = styled(motion.div)`
+  position: relative;
+  display: flex;
+  white-space: nowrap;
+  overflow: hidden;
+  cursor: pointer;
+  margin-left: 50px;
+  a {
+    position: relative;
+    left: 0;
+    top: 0;
+    z-index: 0;
+  }
+`;
+
+export const StyledNavbarLinksContainer = styled(FlexContainer)`
+  gap: ${SPACING[3]};
 `;
